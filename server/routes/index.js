88922c51bb06a796874eeef63e6a2e3d76900452
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-05-26 15:15:42 +0200

    Added code for mongodb connection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
-var _ = require('underscore');
+var _ = require('underscore'),
+	mongo = require('mongodb');
 
 module.exports = {
 	query: function (req, res) {
@@ -9,6 +10,34 @@ module.exports = {
 		console.dir(start);
 		console.dir(limit);
 		console.dir(cfg);
+
+		new mongo.Db('log4net', new mongo.Server('localhost', 27017, {auto_reconnect: true})).open(function(err, db) {
+			if(err) {
+				console.log("error");
+				return;
+			}
+
+			db.collection('logs', {safe:true}, function(err, coll) {
+				if(err) {
+					console.log("error");
+					db.close();
+					return;
+				}
+
+				coll.find().toArray(function (err, docs) {
+					if(err) {
+						console.log("error");
+						db.close();
+						return;
+					}
+
+					console.dir(docs);
+
+					db.close();
+				});
+
+			});
+		});
 
 		var rows = _(_.range(start, start + limit)).map(function (i) {
 			var row = {};
